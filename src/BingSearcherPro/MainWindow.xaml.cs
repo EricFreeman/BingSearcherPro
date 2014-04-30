1460177af128822c4e7f1f552a7432bfb0959de8
Eric Freeman <eric.freeman@extendhealth.com>
2014-04-30 07:49:34 -0600

    moved to src folder and start of actual work on this

---
+++ src/BingSearcherPro/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using BingSearcherPro.Models;
+
+namespace BingSearcherPro
+{
+    public partial class MainWindow
+    {
+        public List<Account> AccountList { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Browser.Navigate("http://www.bing.com");
+        }
+    }
+}
---
+++ src/BingSearcherPro/Models/Account.cs
@@ -0,0 +1,30 @@
+﻿using BingSearcherPro.Util;
+
+namespace BingSearcherPro.Models
+{
+    public class Account : NotifyPropertyBase
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
