168f17a8cbb6f4aea457a5c0eabf6004420b9825
Eric Freeman <eric.freeman@extendhealth.com>
2014-05-03 20:10:11 -0600

    random left over stuff I wrote on the train.  just commiting to continue work on my desktop.

--- src/BingSearcherPro/MainWindow.xaml.cs
+++ src/BingSearcherPro/MainWindow.xaml.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using BingSearcherPro.Models;
 
 namespace BingSearcherPro
 {
-    public partial class MainWindow
+    public partial class MainWindow : INotifyPropertyChanged
     {
-        public List<Account> AccountList { get; set; }
+        private List<Account> _accountList;
+
+        public List<Account> AccountList
+        {
+            get { return _accountList; }
+            set
+            {
+                _accountList = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
             Browser.Navigate("http://www.bing.com");
+
+            AccountList = new List<Account>();
+            AccountList.Add(new Account {UserName = "Bob", Password = "123"});
+            Accounts.ItemsSource = AccountList;
         }
+
+        #region Notify Property Changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
